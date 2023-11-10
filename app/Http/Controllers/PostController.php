4d7ab2656cc8c3c6034ec7f537ee07a9390fd6c6
Marwan <marwahpinrang6@gmail.com>
2023-11-10 15:06:23 +0800

    first commit & insert data kedatabase

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get all posts
+        $posts = Post::latest()->get();
+
+        //return view
+        return inertia('Posts/Index', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return inertia('Posts/Create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //set validation
+        $request->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        //create post
+        Post::create([
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect
+        return redirect()->route('posts.index')->with('success', 'Data Berhasil Disimpan!');
+    }
+}
